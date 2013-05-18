62ee528529d03299b10601b49eed753bf0fa4429
Tim Ullrich <timothy.ullrich@gmail.com>
2013-05-18 15:48:07 -0500

    adding TrianglePrimitive intersect

--- src/primitive.cpp
+++ src/primitive.cpp
@@ -4,7 +4,15 @@ namespace raytracer {
 	
 bool TrianglePrimitive::intersects(const Ray &r,  TraceResult &result)
 {
-	
+	glm::vec3 intersection(0);
+
+	if (r.intersects(triangle, intersection))
+	{
+		result = TraceResult(intersection, triangle, entity, mesh, material);
+		return true;
+	}
+
+	return false;
 }
 
 } /* namespace raytracer */
