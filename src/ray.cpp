42fe6983a97963e9b43de468a9c6111b60a3c2ad
Tim Ullrich <timothy.ullrich@gmail.com>
2013-05-19 13:06:41 -0500

    adding Ray vs. AABB

--- src/ray.cpp
+++ src/ray.cpp
@@ -1,3 +1,4 @@
+#include <cfloat>
 #include "ray.h"
 
 namespace raytracer {
@@ -14,6 +15,8 @@ Ray::Ray(glm::vec3 p, glm::vec3 q)
 {
 	this->p = p;
 	this->q = q;
+
+	this-> n = glm::normalize(q - p);
 }
 
 
@@ -67,6 +70,50 @@ bool Ray::intersects(const Triangle &tri, glm::vec4 &intersection) const
 	intersection.z = w * ood;
 	intersection.x = 1.0f - intersection.y - intersection.z;
 
+	return true;
+}
+
+
+bool Ray::intersects(const AABB &a, glm::vec3 &result) const
+{
+	float tmin = 0.0f;
+	float tmax = FLT_MAX; // max float
+
+	for(int i = 0; i < 3; i++)
+	{
+		if(abs(n[i]) < FLOAT_ERROR) // w/ an allowable error
+		{
+			if(p[i] < a.min[i] || p[i] > a.max[i]) // within the plane
+			{
+				return false;
+			}
+		}
+		else
+		{
+			float ood = 1.0f / n[i];
+			float t1 = (a.min[i] - p[i]) * ood; // intersection with max
+			float t2 = (a.max[i] - p[i]) * ood; // intersection with min
+
+			if (t1 > t2) // ensure t1 is the "close" plane, the plane of the slab first intersected by the ray
+			{
+				float temp = t1;
+				t2 = t1;
+				t1 = temp;
+			}
+
+
+			if (t1 > tmin)
+				tmin = t1;	// furtherst intersection from a "close" plane
+
+			if (t2 > tmax)
+				tmax = t2; // furtherst intersection from a "far" plane
+
+			if (tmin > tmax)
+				return false;
+		}
+	}
+
+	result = p + tmin * n;
 	return true;
 }
 
--- src/ray.h
+++ src/ray.h
@@ -2,6 +2,7 @@
 #define _RAY_H_ 
 
 #include "common.h"
+#include "aabb.h"
 
 namespace raytracer {
 
@@ -36,15 +37,25 @@ public:
 
 	glm::vec3 p; // ray origin point
 	glm::vec3 q; // Direction of the ray is normalize (q - p);
+	glm::vec3 n; // Direction of the ray is normalize (q - p);
 
 	/**
 	 * Calculate the barycentric coordinates of the intersection of this ray with
 	 * {@link Triangle} T.
 	 * @param  t triangle to test intersection
-	 * @param  intersection empty vec3 returning the barycentric coordinates of the intersection with respect to t
+	 * @param  result vec4 returning the barycentric coordinates of the intersection with respect to t
 	 * @return   true if this {@link Ray} intersects t, false otherwise
 	 */
-	bool intersects(const Triangle &t, glm::vec4 &intersection) const;
+	bool intersects(const Triangle &t, glm::vec4 &result) const;
+
+
+	/**
+	 * Intersect ray with an {@AABB link}
+	 * @param  t triangle to test intersection
+	 * @param  result vec3 returning the intersecting point
+	 * @return   true if this {@link Ray} intersects t, false otherwise
+	 */
+	bool intersects(const AABB &aabb, glm::vec3 &result) const;
 };
 
 } /* raytracer */
