1285a28239bf1d284c594e9ccb6dd6d2b290d2ed
Tim Ullrich <timothy.ullrich@gmail.com>
2013-05-21 12:57:21 -0500

    fixing broken camera

--- src/camera.cpp
+++ src/camera.cpp
@@ -4,9 +4,9 @@
 
 namespace raytracer {
 
-Camera::Camera()
+Camera::Camera(int x_res, int y_res)
 {
-	this->eye       = glm::vec3(20.0f, 20.0f, 20.0f); //  position; // 
+	this->eye       = glm::vec3(10.0f, 10.0f, 10.0f); //  position; // 
 	this->lookAt    = glm::vec3(0.0f, 2.0f, 0.0f); // lookAt; // 
 	this->up        = glm::vec3(0.0f, 1.0f, 0.0f); // up; // 
 	
@@ -15,6 +15,16 @@ Camera::Camera()
 	this->FOV       = 90.0f * M_PI / 180.0f ; // in radians
 	this->aspect    = 16.0f / 9.0f;
 	this->FOVy      = FOV / (aspect); // in radians
+
+	this->x_res = x_res;
+	this->y_res = y_res;
+
+	float L_x = 2.0f * tan(FOV/2.0f) * nearPlane; // horizontal length of near plane
+	float L_y = 2.0f * tan(FOV/2.0f) * nearPlane; // vertical length of near plane
+
+	this->pixel_x  = L_x / x_res;// horizontal length of each pixel
+	this->pixel_y  = L_y / y_res;// vertical length of each pixel
+
 	
 	glm::vec3 w     = glm::normalize(eye - lookAt);
 	glm::vec3 u     = glm::normalize(glm::cross(up, w));
@@ -44,23 +54,28 @@ Camera::Camera(const glm::vec3 &position, const glm::vec3 &lookAtPoint, const gl
     this->cameraToWorld = glm::mat4(glm::vec4(u, 0.0f), glm::vec4(v, 0.0f), glm::vec4(w, 0.0f), glm::vec4(t, 1.0f));
 }
 
-void Camera::getPixelCenter(int i, int j, int x_res, int y_res, glm::vec3 &out)
+void Camera::getPixelCenter(int i, int j, glm::vec3 &out, float variance_i, float variance_j) const
 {
-	float near = 150.0f;
-	float L_x = 2.0f * tan(FOV/2.0f) * near; // horizontal length of near plane
-	float L_y = 2.0f * tan(FOV/2.0f) * near; // vertical length of near plane
-	//std::cout << "L_x" << L_x << std::endl;
+	float center_x_i = (pixel_x * (i + 0.5f)) - ((pixel_x * x_res) / 2);//pixel_x * ((2.0f * i + 1.0f) / (2.0f * x_res) - 0.5f);
+	center_x_i += variance_i;
 
+	float center_y_j = (pixel_y * (j + 0.5f)) - ((pixel_y * y_res) / 2);//pixel_y * ((2.0f * j + 1.0f) / (2.0f * y_res) - 0.5f);
+	center_y_j += variance_j;
 
-	float pixel_x  = L_x / x_res;// horizontal length of each pixel
-	float pixel_y  = L_y / y_res;// vertical length of each pixel
-	
-	float center_x_i = pixel_x * ((2.0f * i + 1.0f) / (2.0f * x_res) - 0.5f);
-	float center_y_j = pixel_y * ((2.0f * j + 1.0f) / (2.0f * y_res) - 0.5f);
-	glm::vec4 pixel_center(center_x_i, center_y_j, -1.0f, 1.0f);
+	glm::vec4 pixel_center(center_x_i, center_y_j, -nearPlane, 1.0f);
 
 	out = glm::vec3(cameraToWorld * pixel_center);
+}
 
+
+float Camera::pixelXDimension() const
+{
+	return pixel_x;
+}
+
+float Camera::pixelYDimension() const
+{
+	return pixel_y;
 }
 
 
--- src/camera.h
+++ src/camera.h
@@ -11,23 +11,27 @@ namespace raytracer {
 class Camera
 {
 public:
-	Camera();
+	Camera(int x_res, int y_res);
 	Camera(const glm::vec3 &position, const glm::vec3 &lookAt, const glm::vec3 &up, float nearPlane, float farPlane, float FOV, float aspect);
 
 	glm::vec3 eye, lookAt, up, right;
 	float nearPlane, farPlane, FOV, FOVy, aspect, ;
 	glm::mat4 cameraToWorld;
 
+	float pixel_x, pixel_y;
+	float x_res, y_res;
+
 	/**
 	 * Compute the worldspace vector cooresponding to the center of pixel i, j using
 	 * the orientation of this camera.	
 	 * @param i      horizontal pixel number
 	 * @param j      vertical pixel number
-	 * @param x_res  horizontal resolution
-	 * @param y_res  vertical resolution
 	 * @param out    set to the resulting worldspace vector
 	 */
-	void getPixelCenter(int i, int j, int x_res, int y_res, glm::vec3 &out);
+	void getPixelCenter(int i, int j, glm::vec3 &out, float variance_i, float variance_j)  const;
+
+	float pixelXDimension() const;
+	float pixelYDimension() const;
 };
 
 /**
